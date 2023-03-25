2ac3e9005d12921c8d5ea470158c7764a2b6464d
Khánh Trình Ngô <trinhtit2003@gmail.com>
2023-03-25 22:08:50 +0700

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+class SimpleUdpClient
+{
+    public static void Main()
+    {
+        byte[] data = new byte[1024];
+        string input, stringData;
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        string welcome = "Hello server";
+        data = Encoding.ASCII.GetBytes(welcome);
+        server.SendTo(data, data.Length, SocketFlags.None, ipep);
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+        EndPoint Remote = (EndPoint)sender;
+        data = new byte[1024];
+        int recv = server.ReceiveFrom(data, ref Remote);
+        Console.WriteLine("Thong diep duoc nhan tu {0}:", Remote.ToString());
+        Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+        while (true)
+        {
+            input = Console.ReadLine();
+            if (input == "exit")
+                break;
+            server.SendTo(Encoding.ASCII.GetBytes(input), Remote);
+            data = new byte[1024];
+            recv = server.ReceiveFrom(data, ref Remote);
+            stringData = Encoding.ASCII.GetString(data, 0, recv);
+            Console.WriteLine("Da gui tin nhan thanh cong, Mo tab SERVER de kiem tra.");
+        }
+        Console.WriteLine("Dang dong client");
+        server.Close();
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+class SimpleUdpSrvr
+{
+    public static void Main()
+    {
+        int recv;
+        byte[] data = new byte[1024];
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 5000);
+        Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        newsock.Bind(ipep);
+        Console.WriteLine("Dang cho Client ket noi den...");
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+        EndPoint Remote = (EndPoint)(sender);
+        recv = newsock.ReceiveFrom(data, ref Remote);
+        Console.WriteLine("Thong diep duoc nhan tu {0}:", Remote.ToString());
+        Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+        string welcome = "Hello Client";
+        data = Encoding.ASCII.GetBytes(welcome);
+        newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+        while (true)
+        {
+            data = new byte[1024];
+            recv = newsock.ReceiveFrom(data, ref Remote);
+            Console.WriteLine("Da nhan duoc tin nhan: {0}", Encoding.ASCII.GetString(data, 0, recv));
+            newsock.SendTo(data, recv, SocketFlags.None, Remote);
+        }
+    }
+}
